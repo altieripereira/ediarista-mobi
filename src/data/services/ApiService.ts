e651e1379a0acc170ba4e5ab75729cf3952ce8f7
Altieri Pereira <altieri@loglabdigital.com.br>
2021-11-11 23:03:44 -0400

    fix: bugs no theme

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://127.0.0.1:8000";
+const url = "https://salty-citadel-55102.herokuapp.com";
 
 export const ApiService = axios.create({
   baseURL: url,
